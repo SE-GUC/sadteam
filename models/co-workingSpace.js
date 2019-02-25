ed5bb03b3e5d09948ea7521694f921e8593ec3a1
MoStA09 <47123677+MoStA09@users.noreply.github.com>
2019-02-25 04:44:10 +0200

    Add files via upload

---
+++ models/co-workingSpace.js
@@ -0,0 +1,11 @@
+class coworkingSpace{
+  constructor(name,address,branches,description,dateOfFoundation,gallery){
+    this.name = name;
+    this.address = address;
+    this.branches =branches;
+    this.description = description;
+    this.dateOfFoundation = dateOfFoundation;
+    this.gallery = gallery;
+  };
+
+};
