99a2430bf4188bb1769d7723500e50e094ce4316
MoStA09 <47123677+MoStA09@users.noreply.github.com>
2019-02-25 04:29:44 +0200

    Add files via upload

---
+++ models/consultancyAgency.js
@@ -0,0 +1,12 @@
+class consultancyAgency{
+  constructor(name,address,yearOfFoundation,owner,field,partners,achievements){
+    this.name = name;
+    this.address = address;
+    this.yearOfFoundation = yearOfFoundation;
+    this.owner = owner;
+    this.field = field;
+    this.partners = partners;
+    this.achievements = achievements;
+
+  }
+}
