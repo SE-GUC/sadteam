47833362a6cb7898dea8f5709f302febdfde45e5
MoStA09 <47123677+MoStA09@users.noreply.github.com>
2019-02-25 04:03:24 +0200

    Add files via upload

---
+++ models/member.js
@@ -0,0 +1,16 @@
+class member {
+  constructor(firstName,middleName,lastName,age,address,birthDate,email,password,educationalBackground,
+    skills,protofolio){
+      this.firstName = firstName;
+      this.middleName = middleName;
+      this.lastName = lastName;
+      this.age = age;
+      this.address = address;
+      this.birthDate = birthDate;
+      this.email = email;
+      this.password = password;
+      this.educationalBackground = educationalBackground;
+      this.skills = skills;
+      this.protofolio = protofolio;
+    }
+}
