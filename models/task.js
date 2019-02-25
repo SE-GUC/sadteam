e49afe35db1e6fdb8062c4901db9a46a9ca49daa
MoStA09 <47123677+MoStA09@users.noreply.github.com>
2019-02-25 03:47:02 +0200

    Add files via upload

---
+++ models/task.js
@@ -0,0 +1,15 @@
+class Task{
+  constructor(estimatedEffortAndTime,levelOfCommitment,experienceLevel,monetaryCompansation,partnerWhoOwnsIt,
+    consultancy,setOfSkills,otherRequirments){
+      this.estimatedEffortAndTime = estimatedEffortAndTime;
+      this.levelOfCommitment = levelOfCommitment;
+      this.experienceLevel = experienceLevel;
+      this.monetaryCompansation = monetaryCompansation;
+      this.partnerWhoOwnsIt = partnerWhoOwnsIt;
+      this.consultancy = consultancy;
+      this.setOfSkills = setOfSkills;
+      this.otherRequirments = otherRequirments;
+    };
+  
+
+};
