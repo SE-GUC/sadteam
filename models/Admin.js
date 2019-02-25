6e5683c18ce86fa1b968186ddef86c10adfd18f3
bishoy2018 <bishoraafat10@gmail.com>
2019-02-25 16:47:52 +0200

    added task and admin modules

---
+++ models/Admin.js
@@ -0,0 +1,10 @@
+// Admin Model
+
+class Admin {
+	constructor(username, password, firstName, secondName){
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.secondName = secondName;
+	};
+}
