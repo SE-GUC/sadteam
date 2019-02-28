9de5607ccfb54b5f7f08263039b5bd06cd195d16
Wyoma <mariam.elshrif@gmail.com>
2019-02-28 16:20:00 +0200

    Creating Task.js
    Added basic class framework for the model 'Task'

---
+++ models/Task.js
@@ -0,0 +1,10 @@
+class Task {
+    constructor(taskName, dateCreated, taskDescription)
+    {
+        this.taskName = taskName;
+        this.dateCreated = dateCreated;
+        this.taskDescription = taskDescription;
+    }
+}
+
+module.exports = Task;
