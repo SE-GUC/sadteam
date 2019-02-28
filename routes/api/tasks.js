2c596e79d90a72b51e432419d8d81b41e9b4966b
Wyoma <mariam.elshrif@gmail.com>
2019-02-28 17:08:45 +0200

    Adjusted task constructor to correct format

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const Task = require('../../models/Task');
 
 const tasks = [
-	new Task('Task1', 5),
-	new Task('Task2', 2),
-	new Task('Task3', 7),
-	new Task('Task4', 12),
-	new Task('Task5', 10)
+	new Task('Task1', 'description1'),
+	new Task('Task2', 'description2'),
+	new Task('Task3', 'description3'),
+	new Task('Task4', 'description4'),
+	new Task('Task5', 'description5')
 ];
 
 router.get('/', (req, res) => res.json({ data: tasks }));
