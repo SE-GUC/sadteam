04c235341ef6f8444b030672be5b6ea420e61775
Wyoma <mariam.elshrif@gmail.com>
2019-02-28 16:38:12 +0200

    Adding an 'id' attribute

--- models/Task.js
+++ models/Task.js
@@ -1,9 +1,12 @@
+const uuid = require('uuid');
+
 class Task {
     constructor(taskName, dateCreated, taskDescription)
     {
         this.taskName = taskName;
         this.dateCreated = dateCreated;
         this.taskDescription = taskDescription;
+        this.id = uuid.v4()
     }
 }
 
