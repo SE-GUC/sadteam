b8e650d75e9e6f11b51a7399227d32d1fd8b27a6
Ahmed Ashraf <47159033+ahmed-enawy@users.noreply.github.com>
2019-03-01 02:51:41 +0200

    Update user.js

--- routes/API/user.js
+++ routes/API/user.js
@@ -25,42 +25,34 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => { 
 	insertUser(req,res);
 });
-
 //**4'** the create/insert function for user !
-
 function insertUser(req,res){
-	var user = new User();
-	user.firstName = req.body.firstName;
-	user.middleName = req.body.middleName;
-	user.lastName = req.body.lastName;
-	user.age = req.body.age;
-	user.birthDate = req.body.birthDate;
-	user.email = req.body.email;
-	user.password = req.body.password;
-	user.educationalBackground = req.body.educationalBackground;
-	user.skills = req.body.skills;
-	user.protofolio = req.body.protofolio;
-	user.save((err, doc) => {
-		if(!err)
-			res.redirect('user/records'); // if no err while inserting, redirect to new route
-		else{ //for validation error 3
-			if(err.name =='ValidationError'){
-				handleValidationError(err, req.body);
-				res.render("user/addUser", {
-					viewTitle: "Insert User", 
-					user : req.body
+	var user = new User(req.body);
+	var error = user.Validating(req.body);
+	if(error.error!=null){
+		res.json(error);
+	}else{
+		User.find({email:req.body.email}, function(err,user1){
+			if(user1.length!=0){
+				res.json("Email Address Already Exists")
+			}else{
+				user.save((err, doc) => {
+					if(!err)
+						res.redirect('../user/records'); // if no err while inserting, redirect to new route
+					else{ 
+						console.log('Error while inserting user: ' + err);
+					}
 				});
 			}
-			else
-			console.log('Error while inserting user: ' + err);
-		}
-	});
+		});
+	
+}
 }
 
 //creating new route to show inserted user records
 router.get('/records', (req,res) => {
 	User.find().
-	select('id firstName middleName lastName age birtDate email password educationalBackground Skills  portofolio') // selecting the attributes to be shown
+	select('id firstName middleName lastName age birtDate email password educationalBackground Skills  portofolio')
 	.then(users => res.json(users));
 });
 
@@ -74,7 +66,6 @@ router.route('/records/:email').get(function(req,res){
 	;
 	
 });
-// updating an existing account
 router.route('/update/:email').post(function(req, res) {
     User.find({email: req.params.email},function(err,user){
 		if(user.length==0){
@@ -114,7 +105,7 @@ router.route('/update/:email').post(function(req, res) {
             }
             if(!req.body.Tasks){
             }else{
-                user[0].protofolio = req.body.protofolio;
+                user[0].portofolio = req.body.portofolio;
             }
 		}
 		user[0].save((err, doc) => {
@@ -138,46 +129,7 @@ router.route('/:email/remove').post(function(req, res) {
 	})
 }
 );
-//vaildation error msg saved in body
-function handleValidationError(err, body){
-	for(field in err.errors)
-	{
-		switch(err.erros[field].path){ //fields are firstName, middleName, lastName......
-			case 'firstName':
-				body['firstNameError'] = err.errors[field].message; //this field is required
-				break;
-			case 'middleName':
-				body['middleNameError'] = err.errors[field].message; //this field is required
-				break;
-			case 'lastName':
-				body['lastNameError'] = err.errors[field].message; //this field is required
-				break;
-			case 'age':
-				body['ageError'] = err.errors[field].message; //this field is required
-				break;
-			case 'birthDate':
-				body['birthDateError'] = err.errors[field].message; //this field is required
-				break;
-			case 'email':
-				body['emailError'] = err.errors[field].message; //this field is required
-				break;
-			case 'password':
-				body['passwordError'] = err.errors[field].message; //this field is required
-				break;
-			case 'educationalBackground':
-				body['educationalBackgroundError'] = err.errors[field].message; //this field is required
-				break;
-			case 'skills':
-				body['skillsError'] = err.errors[field].message; //this field is required
-				break;
-			case 'protofolio':
-				body['protofolioError'] = err.errors[field].message; //this field is required
-				break;
-			default:
-				break;
-		}
-	}
-}
+
 
 
 module.exports = router; //exporting this router object
