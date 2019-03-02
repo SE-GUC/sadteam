d5ee1a4fa1f20e806b0318f7f1d9d10085a2adf1
Syajel <32969749+Syajel@users.noreply.github.com>
2019-03-02 22:35:14 +0200

    Added create functionality

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -17,6 +17,42 @@ const tasks = [
 
 router.get('/', (req, res) => res.json({ data: tasks }));
 
+router.post('/', (req, res) => {
+    const taskName = req.body.taskName;
+	const taskDescription = req.body.taskDescription;
+
+    if (!taskName) return res.status(400).send({ err: 'taskName field is required' });
+    if (typeof taskName !== 'string') return res.status(400).send({ err: 'Invalid value for taskName' });
+
+    const newTask = {
+        taskName,
+		taskDescription,
+        id: uuid.v4(),
+    };
+    return res.json({ data: newTask });
+});
+
+router.post('/joi', (req, res) => {
+    const taskName = req.body.taskName
+	const taskDescription = req.body.taskDescription;
+
+    const schema = {
+        taskName: Joi.string().min(3).required(),
+		taskDescription: Joi.string()
+    }
+
+    const result = Joi.validate(req.body, schema);
+
+    if (result.error) return res.status(400).send({ error: result.error.details[0].message });
+
+    const newTask = {
+        taskName,
+		taskDescription,
+        id: uuid.v4(),
+    };
+    return res.json({ data: newTask });
+});
+
 app.put('/api/tasks/:id', (req, res) => {
     const taskID = req.params.id 
     const updatedName = req.body.name
