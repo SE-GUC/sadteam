8c611f2dfb8b0bbbdff0d254899c952a8e25ea01
MoStA09 <47123677+MoStA09@users.noreply.github.com>
2019-03-02 16:32:39 +0200

    Create masterclass.js
    A model for master class entity

---
+++ models/masterclass.js
@@ -0,0 +1,12 @@
+class masterclass{
+  constructor(name,projects,workshops,courses,id){
+    this.name = name
+    this.projects = projects
+    this.workshops = workshops 
+    this.courses = courses
+    this.id = id
+
+  }
+}
+
+module.exports = masterclass
