2e826afb18fa9bc0b6fa43fabcb7721857c73de7
Syajel <32969749+Syajel@users.noreply.github.com>
2019-03-02 22:14:59 +0200

    Changing link names to new correct links

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
    
 const express = require('express')
-const masterclass = require('./routes/api/masterclass')
+const masterclass = require('./routes/api/masterclasses')
 const app = express()
 app.use(express.json())
 
-app.use('/api/masterclass', masterclass)
+app.use('/api/masterclasses', masterclass)
 
 app.use((req, res) => {
     res.status(404).send({err: 'We can not find what you are looking for'});
