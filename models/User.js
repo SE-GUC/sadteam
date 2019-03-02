740cdcba387bd299c9b27956e076000e2f803bb1
Ahmed Ashraf <47159033+ahmed-enawy@users.noreply.github.com>
2019-03-02 02:14:32 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -6,64 +6,41 @@ const mongoose = require('mongoose');
 var userSchema = new mongoose.Schema({
 	firstName: {
 		type: String,
-		required: 'This field is required' //valdiation error msg
 	},
 	middleName: {
 		type: String,
-		required: 'This field is required'
 	},
 	lastName: {
 		type: String,
-		required: 'This field is required'
 	},
 	age: {  // we may make it String.. both are okay!
 		type: Number,
 		min: 8,
-		required: 'This field is required'
 	},
 	birthDate: {
 		type: Date,
-		required: 'This field is required'
 	},
 	email: {
 		type: String,
-		required: 'This field is required'
 	},
 	password: {
 		type: String,
-		required: 'This field is required'
 	},
 	educationalBackground: {
 		type: String,
-		required: 'This field is required'
 	},
 	skills: {
 		type: String,
-		required: 'This field is required'
 	},
-	protofolio: {
+	portofolio: {
 		type: String,
-		required: 'This field is required'
 	}
 });
 
-//validation 2
-// custom validation for email
-/*
-userSchema.path('email').validate((val) => {
-	emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return emailRegex.test(val);
-}, 'Invalid e-mail');
-*/
-/*
-function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}*/
 
 
-//as a first parameter we will spicify the name of schema then we have the schema object
-mongoose.model('User', userSchema);
+
+
 
 
 
