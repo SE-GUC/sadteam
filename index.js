7a1a96eaa9d13756ef13754572f097699b8b2369
Wyoma <mariam.elshrif@gmail.com>
2019-03-23 17:53:29 +0200

    Added Mongoose connection and extra hyperlinks

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 require('./models/db');
 const users = require('./routes/api/user');
 const bodyparser = require('body-parser');
@@ -7,17 +7,22 @@ const tasks = require('./routes/api/tasks')
 const masterclasses = require('./routes/api/masterclasses')
 const app = express()
 app.use(express.json())
-app.use('/user', users);
 app.use(bodyparser.json());
+const db = require('./config/keys').mongoURI
+mongoose
+    .connect(db)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err))
+
 
 app.get('/', (req, res) => {
-    res.send(`<h1>Lirten Hub</h1>
-    <a href="/api/tasks">Tasks</a>
-    <a href="/api/users">Users</a>
-     <a href="/api/masterclasses">Masterclasses</a>
-    `);
-})
-
+        res.send(`<h1>Lirten Hub</h1>
+        <a href="/api/tasks">Tasks</a>
+        <a href="/api/users">Users</a>
+         <a href="/api/masterclasses">Masterclasses</a>
+        `);
+    })
+    
 app.use('/api/tasks', tasks)
 app.use('/api/users', users)
 app.use('/api/masterclasses', masterclasses)
@@ -26,6 +31,6 @@ app.use((req, res) => {
     res.status(404).send({err: 'We can not find what you are looking for'});
  })
 
-const port = 3000
+ const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Server up and running on port ${port}`))
 
