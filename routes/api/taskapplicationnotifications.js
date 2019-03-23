eba8e827499b3ff25a533dc97ced328bab7e318f
bishoy2018 <bishoraafat10@gmail.com>
2019-03-23 21:47:02 +0200

    create notification function for task application

---
+++ routes/api/taskapplicationnotifications.js
@@ -0,0 +1,39 @@
+/* Waiting for front end...
+
+const express = require('express');
+var router = express.Router();
+const mongoose = require('mongoose');
+const TaskApplicationNotif = mongoose.model('TaskApplicationNotif'); 
+
+*/
+
+
+
+/*router.post('', (req, res) => { 
+
+	insertTaskAppNotif(req,res);
+
+});*/
+
+
+/* 
+//CREATE NOTIFICATION FUNCTION
+
+function insertTaskAppNotif(req,res){
+	var taskAppNotif = new TaskApplicationNotif();
+	taskAppNotif.targetUser = req.body.targetUser;
+	taskAppNotif.notifText = req.body.notifText;
+	taskAppNotif.taskName = req.body.taskName;
+}*/
+
+
+/*router.get('', (req,res) => { 
+
+
+});*/
+
+
+
+
+
+//module.exports = router; 
