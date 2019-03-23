27536da3e9e261dd0ad1d4249c2ac041c1e544f9
Syajel <32969749+Syajel@users.noreply.github.com>
2019-03-23 16:09:51 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -36,27 +36,10 @@ var userSchema = new mongoose.Schema({
 		type: String,
 	}
 });
-const joi = require('joi');
-userSchema.methods.Validating = function(obj){
-	var schema = {
-		firstName: joi.string().required().min(3).max(20),
-		middleName: joi.string().required().min(3).max(20),
-		lastName: joi.string().required().min(3).max(20),
-		age: joi.number().required().min(18),
-		birthDate: joi.date().required(),
-		email: joi.string().email().required(),
-		password: joi.string().required().regex(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/),
-		educationalBackground: joi.string().required().min(5),		
-		skills: joi.string().required().min(5),
-		portofolio: joi.string().required()
-
-	}
-	return joi.validate(obj,schema,{abortEarly: false });
-}
 
 //as a first parameter we will spicify the name of schema then we have the schema object
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = User = mongoose.model('users', userSchema);
 
 
 
