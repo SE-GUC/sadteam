5dc40749705dea8730ab01ebeed27c6549ae665a
Wyoma <mariam.elshrif@gmail.com>
2019-03-23 16:15:49 +0200

    Added User and Masterclass hyperlinks to our homepage

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const express = require('express')
 
 require('./models/db');
-const userController = require('./routes/api/user');
+const users = require('./routes/api/user');
 const bodyparser = require('body-parser');
 const tasks = require('./routes/api/tasks')
+const masterclasses = require('./routes/api/masterclasses')
 const app = express()
 app.use(express.json())
-app.use('/user', userController);
+app.use('/user', users);
 app.use(bodyparser.json());
 
 app.get('/', (req, res) => {
     res.send(`<h1>Lirten Hub</h1>
     <a href="/api/tasks">Tasks</a>
+    <a href="/api/users">Users</a>
+     <a href="/api/masterclasses">Masterclasses</a>
     `);
 })
 
 app.use('/api/tasks', tasks)
+app.use('/api/users', users)
+app.use('/api/masterclasses', masterclasses)
 
 app.use((req, res) => {
     res.status(404).send({err: 'We can not find what you are looking for'});
