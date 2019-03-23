fd886c2cdbdb6f88735052695ae0e7df58eb5dd5
Syajel <32969749+Syajel@users.noreply.github.com>
2019-03-23 22:59:15 +0200

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const User = require('./User').Schema
+const User = require('./User').schema
 
 
 const TaskSchema = new Schema({
