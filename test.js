7143005b29da58dd229b12e944459f81ecafa4ba
Wyoma <mariam.elshrif@gmail.com>
2019-03-31 20:25:24 +0200

    Added update user test

--- test.js
+++ test.js
@@ -13,3 +13,18 @@ test('Update a task', async () => {
   await axios.delete(`http://localhost:3000/api/task/${task.data.data._id}`)
   expect(updatedTask.status).toEqual(200)
 });
+
+test('Update a user', async () => {
+  body ={
+      "firstName": "John",
+      "lastName": "Doe",
+      "age": 12,
+      "email": "someone@something.com",
+      "password": "12345"
+      }
+  expect.assertions(1)
+  const user = await axios.post("http://localhost:3000/api/users",body)
+  const updatedUser = await axios.put(`http://localhost:3000/api/users/${user.data.data._id}`,{"age": 16})
+  await axios.delete(`http://localhost:3000/api/user/${user.data.data._id}`)
+  expect(updatedUser.status).toEqual(200)
+});
