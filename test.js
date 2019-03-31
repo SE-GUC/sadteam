baa63df176947c2eac7b72ff5292dda00ffd2a4d
Wyoma <mariam.elshrif@gmail.com>
2019-03-31 21:56:55 +0200

    Added test for updating consultancy agency info

--- test.js
+++ test.js
@@ -146,17 +146,18 @@ test('Adding user as an applicant to a task', async () => {
     expect(task.data.name).toEqual('Task1')
   });
 
-
-test('Delete a task', async () => {
+test('Updating consultancy agency info', async () => {
   body ={
-      "name": "taskName",
-      "description": "taskDescription",
-      "skillsRequired": ["Heraldry"],
-      "currentState": "Created",
-      "reviewed": true
+      "firstName": "John",
+      "lastName": "Doe",
+      "age": 12,
+      "email": "someone@something.com",
+      "password": "12345"
+      "consultancyAgency": true
       }
   expect.assertions(1)
-  const task = await axios.post("http://localhost:3000/api/tasks",body)
-  const deletedTask = await axios.delete(http://localhost:3000/api/tasks/${task.data.data._id})
-  expect(deletedTask.status).toEqual(200)
+  const user = await axios.post("http://localhost:3000/api/users",body)
+  const updatedUser = await axios.put("http://localhost:3000/api/users/${user.data.data._id}",{"consultancyInfo": "NewInfo"})
+  await axios.delete("http://localhost:3000/api/users/${user.data.data._id}")
+  expect(updatedUser.status).toEqual(200)
 });
