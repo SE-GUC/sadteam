52ee287d6d0aed33877055da8f84cd4b74caa6da
bishoy2018 <bishoraafat10@gmail.com>
2019-03-31 14:33:18 +0200

    some helpful funtions; about to start testing

---
+++ routes/testing/functions.test.js
@@ -0,0 +1,9 @@
+
+// -- some helpful functions
+
+// CHECK FOR TRUTHY AND FALSY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined 
+// toBeTruthy matches anything that an if statement treats as ture
+// toBeFlasy matches anything that an if statement treats as false
