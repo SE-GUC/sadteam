3adb791c46458af9b934685358228a005bdb8bff
bishoy2018 <bishoraafat10@gmail.com>
2019-03-31 14:48:36 +0200

    just trying some demo functions to know more about jest

--- routes/testing/functions.test.js
+++ routes/testing/functions.test.js
@@ -7,3 +7,10 @@
 // toBeDefined 
 // toBeTruthy matches anything that an if statement treats as ture
 // toBeFlasy matches anything that an if statement treats as false
+
+const functions = require('./justDemo');
+
+test('add 2 + 2 to equal 4', () =>{
+	expect(functions.add(2,2)).toBe(4);
+});
+
