c353314153779738b9f807a6364dff818dd8bbda
Syajel <32969749+Syajel@users.noreply.github.com>
2019-04-04 12:28:52 +0200

    Update masterclasses.js

--- routes/api/masterclasses.js
+++ routes/api/masterclasses.js
@@ -84,18 +84,18 @@ router.post('/joi', (req, res) => {
 
 
 router.get('/', async (req,res) => {
-    const masterclasses = await Masterclass.find()
+    const masterclasses = await masterclasses.find()
     res.json({data: masterclasses})
 })
 
 router.put('/:id', async (req,res) => {
     try {
      const id = req.params.id
-     const masterclass = await Masterclass.findOne({id})
+     const masterclass = await masterclasses.findOne({id})
      if(!masterclass) return res.status(404).send({error: 'Masterclass does not exist'})
      const isValidated = validator.updateValidation(req.body)
      if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
-     const updatedMasterclass = await Masterclass.updateOne(req.body)
+     const updatedMasterclass = await masterclasses.updateOne(req.body)
      res.json({msg: 'Masterclass updated successfully'})
     }
     catch(error) {
@@ -107,7 +107,7 @@ router.put('/:id', async (req,res) => {
 router.delete('/:id', async (req,res) => {
     try {
      const id = req.params.id
-     const deletedMasterclass = await Masterclass.findByIdAndRemove(id)
+     const deletedMasterclass = await masterclasses.findByIdAndRemove(id)
      res.json({msg:'Masterclass was deleted successfully', data: deletedMasterclass})
     }
     catch(error) {
@@ -120,7 +120,7 @@ router.post('/', async (req,res) => {
     try {
      const isValidated = validator.createValidation(req.body)
      if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
-     const newMasterclass = await Masterclass.create(req.body)
+     const newMasterclass = await masterclasses.create(req.body)
      res.json({msg:'Masterclass was created successfully', data: newMasterclass})
     }
     catch(error) {
