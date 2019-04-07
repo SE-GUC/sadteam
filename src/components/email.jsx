3c9486fcf27c228d1fe756d09f1f2672e3b16dfa
Ahmed Ashraf <47159033+ahmed-enawy@users.noreply.github.com>
2019-04-07 09:53:30 +0200

    email componenet

---
+++ src/components/email.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "../frontPage.css";
+class Email extends Component {
+  render() {
+    return (
+      <div>
+        <span className="Email text-muted font-weight-bold font-italic">
+          Email:
+        </span>
+      </div>
+    );
+  }
+}
+
+export default Email;
