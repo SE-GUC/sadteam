e0ba326f80e095d14cbc45d30e9ad353a074c24f
Ahmed Ashraf <47159033+ahmed-enawy@users.noreply.github.com>
2019-04-07 09:39:22 +0200

    masterClasses link for the profile

---
+++ src/components/masterclassess.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class MasterClassess extends Component {
+  render() {
+    return (
+      <div>
+        <a href="" className="text-danger">
+          masterclasses{" "}
+        </a>
+      </div>
+    );
+  }
+}
+
+export default MasterClassess;
