f576cf9f19c11cb7e25e356a0278d62630d88c31
Ahmed Ashraf <47159033+ahmed-enawy@users.noreply.github.com>
2019-04-07 09:14:23 +0200

    the edit profile component

---
+++ src/components/edit.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class Edit extends Component {
+  render() {
+    return (
+      <div>
+        <a href="" className="text-danger">
+          edit{" "}
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Edit;
