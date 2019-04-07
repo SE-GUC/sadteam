6991cd9284f78dc91cab07df1c1a72783f263e3a
Ahmed Ashraf <47159033+ahmed-enawy@users.noreply.github.com>
2019-04-07 09:55:49 +0200

    profilePic component

---
+++ src/components/viewProfilePicture.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+class ViewPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <img className="profilePic" src="123.jpg" />
+      </div>
+    );
+  }
+}
+export default ViewPage;
