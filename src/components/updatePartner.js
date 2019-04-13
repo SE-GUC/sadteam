423681e6725d12a39b1d32e566a314556d0f2e62
bishoy2018 <bishoraafat10@gmail.com>
2019-04-13 18:24:58 +0200

    update partner info

---
+++ src/components/updatePartner.js
@@ -0,0 +1,112 @@
+// -- Update Partner Info
+// -- Business information, partners, board members, events organised, and field of work
+
+import React from "react";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn } from 'mdbreact';
+
+const FormPage = () => {
+  return (
+    <MDBContainer>
+      <MDBRow>
+        <MDBCol md="6">
+          <form>
+            <p className="h4 text-center mb-4 text-primary">Update Info</p>
+            <label htmlFor="defaultFormRegisterNameEx" className="text-primary">
+              Partner Name
+            </label>
+            <input
+              type="text"
+              id="defaultFormRegisterNameEx"
+              className="form-control"
+              placeholder="Enter your name"
+            />
+            <br />
+             <label htmlFor="defaultFormRegisterNameEx" className="text-primary">
+              Business Information
+            </label>
+            <input
+              type="text"
+              id="defaultFormRegisterNameEx"
+              className="form-control"
+              placeholder="Please type your business information"
+            />
+            <br />
+             <label htmlFor="defaultFormRegisterNameEx" className="text-primary">
+              Partners
+            </label>
+            <input
+              type="text"
+              id="defaultFormRegisterNameEx"
+              className="form-control"
+              placeholder="Please enter your partners names"
+            />
+            <br />
+            <label htmlFor="defaultFormRegisterEmailEx" className="text-primary">
+              Board Members
+            </label>
+            <input
+              type="text"
+              id="defaultFormRegisterEmailEx"
+              className="form-control"
+              placeholder="Your board members"
+            />
+            <br />
+            <label htmlFor="defaultFormRegisterEmailEx" className="text-primary">
+              Events Oragnized
+            </label>
+            <input
+              type="email"
+              id="defaultFormRegisterEmailEx"
+              className="form-control"
+              placeholder="Events"
+            />
+            <br />
+            <label
+              htmlFor="defaultFormRegisterConfirmEx"
+              className="text-primary"
+            >
+              Field Of Work
+            </label>
+            <input
+              type="email"
+              id="defaultFormRegisterConfirmEx"
+              className="form-control"
+              placeholder="Write down your work field "
+            />
+            <br />
+            <label htmlFor="defaultFormRegisterNameEx" className="text-primary">
+              Email
+            </label>
+            <input
+              type="email"
+              id="defaultFormRegisterNameEx"
+              className="form-control"
+              placeholder="....@example.com"
+            />
+            <br />
+            <label
+              htmlFor="defaultFormRegisterPasswordEx"
+              className="text-primary"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="defaultFormRegisterPasswordEx"
+              className="form-control"
+              placeholder="Enter your password"
+            />
+            <br />
+            <div className="text-center mt-4">
+              <MDBBtn color="primary" type="submit">
+                Update
+              </MDBBtn>
+            </div>
+          </form>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+};
+
+export default FormPage;
