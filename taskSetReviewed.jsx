4478d5a285463d773cdad8542a50e35749f76a07
Syajel <32969749+Syajel@users.noreply.github.com>
2019-04-14 22:10:15 +0200

    Add files via upload

---
+++ taskSetReviewed.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+class taskSetReviewed extends React.Component {
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            id: "",
+            Reviewed: false
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        axios.put("http://localhost:3000/api/tasks", {
+        id: this.state.id,
+        Reviewed: true
+        })
+    }
+    render() {
+        return (
+            <div className="taskSetReviewed">
+            Task ID: <input type = "text" id = "id"  onChange={e => this.setState({ id: e.target.value })} />
+            <br></br>
+            <button onClick={this.handleClick}>Done</button>   
+            </div>
+            )
+        }
+    }
+export default taskSetReviewed
