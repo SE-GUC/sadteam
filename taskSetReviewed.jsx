04ee3a02968859e20f8136b33860791548179647
Syajel <32969749+Syajel@users.noreply.github.com>
2019-04-14 23:21:16 +0200

    Update taskSetReviewed.jsx

--- taskSetReviewed.jsx
+++ taskSetReviewed.jsx
@@ -11,7 +11,7 @@ class taskSetReviewed extends React.Component {
         this.handleClick = this.handleClick.bind(this);
     }
     handleClick() {
-        axios.put("http://localhost:3000/api/tasks", {
+        axios.put("http://localhost:3000/api/tasks/:id", {
         id: this.state.id,
         Reviewed: true
         })
