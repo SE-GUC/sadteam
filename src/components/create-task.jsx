629ef10fc2fa1428e9c29723ecf378007dbf28ca
Syajel <32969749+Syajel@users.noreply.github.com>
2019-04-14 22:11:43 +0200

    Update create-task.jsx

--- src/components/create-task.jsx
+++ src/components/create-task.jsx
@@ -16,17 +16,18 @@ class CreateTask extends React.Component {
                   name: this.state.name,
                   description: this.state.description
                 })
+              }
   render() {
     return (
       <div className="CreateTask">
          Task Name: <input type = "text" name = "name"  onChange={e => this.setState({ name: e.target.value })} />
-         <br>
+         <br></br>
          Description: <input type = "text" name = "description" onChange={e => this.setState({ description: e.target.value })} />
-         <br>
+         <br></br>
           <button onClick={this.handleClick}>Submit Description For Review</button>   
         </div>
-    );
-  }
+    )
+    };
 }
 
-export default CreateTask;
+export default CreateTask
