8bb2953aba08cc9e5459dd084884ceef236d9567
Wyoma <mariam.elshrif@gmail.com>
2019-04-14 23:38:14 +0200

    bugfix

--- taskSetReviewed.jsx
+++ taskSetReviewed.jsx
@@ -11,10 +11,7 @@ class taskSetReviewed extends React.Component {
         this.handleClick = this.handleClick.bind(this);
     }
     handleClick() {
-        axios.put("http://localhost:3000/api/tasks/:id", {
-        id: this.state.id,
-        Reviewed: true
-        })
+        axios.put("http://localhost:3000/api/tasks/${id}", {"reviewed": true})
     }
     render() {
         return (
