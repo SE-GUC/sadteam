882f1bccbad9c6a4af4c96b482017bfb218c2aeb
Ahmed Ashraf <47159033+ahmed-enawy@users.noreply.github.com>
2019-04-14 16:45:42 +0200

    Update profilePage.jsx

--- src/components/profilePage.jsx
+++ src/components/profilePage.jsx
@@ -1,23 +1,60 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Edit from "./edit";
-import MasterClassess from "./masterclassess";
-import Tasks from "./tasks";
 import Email from "./email";
+import Tasks from "./tasks";
 import ProfilePic from "./uploadProfilePic";
 import Fullname from "./fullName";
 import Skills from "./skills";
+import RecommendTask from "./recommendTaskbutton";
+import Axios from "axios";
 class ProfilePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName:null,
+      middleName:null,
+      lastName:null,
+      FullName: firstName+" "+" "+middleName+" "+lastName+" ",
+      email: props.email, // it should be passed from the login component,
+      tasks: [],
+      user: null,
+      masterclassess: []
+    };
+  }
+  getTasksfromDb() {
+    Axios.get(""api/tasks/")
+      .then(res => res)
+      .then(data => this.setState({ tasks: data }));
+  }
+  getUserfromDb() {
+    Axios.get("api/users/records/" + this.state.email)
+      .then(res => res)
+      .then(data => this.setState({ user: data.user }));
+  }
+  getMasterClassesFromDb() {
+    Axios.get("api/masterclasses")
+      .then(res => res)
+      .then(data => this.setState({ masterclassess: data.masterclassess }));
+  }
+  componentDidMount() {
+    this.getUserfromDb();
+    this.getMasterClassesFromDb();
+    this.getTasksfromDb();
+  }
   render() {
+    const { FullName, email, MasterClassess, tasks, user } = this.state;
     return (
       <div>
         <ProfilePic />
-        <Fullname />
-        <Email />
+        <Fullname name={FullName} />
+        <Email email={email} />
         <Skills />
         <Tasks />
-        <MasterClassess />
+        <MasterClassess masterclassess={MasterClassess} />
         <Edit />
+        <p />
+        <RecommendTask email={email} />
       </div>
     );
   }
